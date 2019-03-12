f9c4c6eb0557c93eeca27de55314282d7adccd64
Skye Leahy <skyeleahy@protonmail.com>
2019-03-11 21:17:40 -0700

    add code for rendering state data using map, conditional rendering, imports exports props, handling click events, destructuring inline styling and keys

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Loading from './Loading';
 
 // component extended from React
 class App extends Component {
@@ -9,42 +10,57 @@ class App extends Component {
     this.state = {
       users: [],
       loading: false
-    }
+    };
+    // bind
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  getUsers() {
-    // when we make the call, set loading to true
-    this.setState({
-      loading: true
-    })
-    // once we get the response, set loading to false
-    axios('https://api.randomuser.me/?nat=US&results=5')
-    //makes API call and sets the state so the users
-    //will have the users data
-      .then(response => 
-        this.setState({
-          users: response.data.results,
-          loading: false
-        })
-      );
-  }
+getUsers() {
+  // when we make the call, set loading to true
+  this.setState({
+    loading: true
+  })
+  // once we get the response, set loading to false
+  axios('https://api.randomuser.me/?nat=US&results=5')
+  //makes API call and sets the state so the users
+  //will have the users data
+    .then(response => 
+      this.setState({
+        users: [...this.state.users, ...response.data.results],
+        loading: false
+      })
+    );
+}
 
-  // just before component is mounted
-  componentWillMount() {
-    this.getUsers();
-  }
+handleSubmit(e) {
+  e.preventDefault();
+  this.getUsers();
+  console.log('more users loaded');
+}
 
-  //using render() method, return JS6
-  render() {
-    return (
+// just before component is mounted
+componentWillMount() {
+  this.getUsers();
+}
+
+//using render() method, return JS6
+render() {
+  const {loading, users} = this.state;
+  return (
       // returning JS6
       // if loading is true, the following will render :)
-      <div className="App">{!this.state.loading ? this.state.users.map(user => (
-          <div>
-            <h3>{user.name.first}</h3>
+      <div className="App">
+      <form onSubmit={this.handleSubmit}>
+        <input type="submit" value="load users" />
+      </form>
+      <hr />
+      {!loading ? users.map(user => (
+          <div key={user.id.value}>
+            <h3 style={{color: 'red'}}>{user.name.first}</h3>
             <p>{user.email}</p>
+            <hr />
           </div>
-        )) : 'Loading . . '}
+        )) : <Loading message="Hey hey hey" />}
       </div>
     );
   }
